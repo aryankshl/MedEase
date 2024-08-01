c115ab09a6711a2a50facad6424a42c00e9d6ea9
Aryan <lit2021037@iiitl.ac.in>
2024-08-01 22:32:53 +0530

    Project Completion

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+import { AppointmentForm } from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+
+const Appointment = async ({ params: { userId } }: SearchParamProps) => {
+  const patient = await getPatient(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      <Image
+        src="/assets/images/medease-bg.jpg"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="absolute z-[-1] h-screen w-full max-w-full blur-md brightness-50"
+      />
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          {/* <Image
+
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          /> */}
+          <h1 className=" mb-2 w-fit text-5xl font-bold text-dark-700">
+            MedEase
+          </h1>
+
+          <AppointmentForm
+            patientId={patient?.$id}
+            userId={userId}
+            type="create"
+          />
+
+          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
+        </div>
+      </section>
+
+      {/* <Image
+        src="/assets/images/appointment-img.png"
+        height={1500}
+        width={1500}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      /> */}
+    </div>
+  );
+};
+
+export default Appointment;
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cd1ce9530aaf35b7e631af7136ac3cd9@o4507602014830592.ingest.us.sentry.io/4507602017583104",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
